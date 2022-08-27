b8e69c5ff66068707c18e2ab8c27a4b01d56b904
jonbenronron <ronijonathanbk@gmail.com>
2022-08-27 13:28:51 +0300

    feat: added goal class

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,24 @@ struct Data {
 }
 
 impl Data {
-    fn rnd () -> Self {
+    fn rnd (
+        class_x_limits: [f32; 2],
+        class_y_limits: [f32; 2]
+    ) -> Self {
         let mut rng = rand::thread_rng();
+        
+        let x = rng.gen::<f32>() * 100.;
+        let y = rng.gen::<f32>() * 100.;
+
         Data {
-            x: [rng.gen::<f32>(), rng.gen::<f32>()],
-            r: rng.gen::<bool>(),
+            x: [x, y],
+            r: is_between(x, class_x_limits) && is_between(y, class_y_limits),
         }
     }
+}
+
+fn is_between (d: f32, interval: [f32; 2]) -> bool {
+    interval[0] < d && d < interval[1]
 }
 
 #[derive(Debug, Clone)]
@@ -23,11 +34,21 @@ struct Class {
 }
 
 impl Class {
+    fn new (
+        x_limits: [f32; 2],
+        y_limits: [f32; 2],
+    ) -> Self {
+        Class {
+            x_limits: x_limits,
+            y_limits: y_limits,
+        }
+    }
+
     fn rnd () -> Self {
         let mut rng = rand::thread_rng();
         
-        let mut x: [f32; 2] = [rng.gen::<f32>(), rng.gen::<f32>()];
-        let mut y: [f32; 2] = [rng.gen::<f32>(), rng.gen::<f32>()];
+        let mut x: [f32; 2] = [rng.gen::<f32>() * 100., rng.gen::<f32>() * 100.];
+        let mut y: [f32; 2] = [rng.gen::<f32>() * 100., rng.gen::<f32>() * 100.];
 
         x.sort_by(|a, b| a.partial_cmp(b).unwrap());
         y.sort_by(|a, b| a.partial_cmp(b).unwrap());
@@ -42,10 +63,16 @@ impl Class {
 type TData = Vec<Data>;
 
 fn main() {
-    // Generate 10 random data points
-    let data: TData = [0; 10].iter().map(|_| Data::rnd()).collect();
+    let goal_x_limits: [f32; 2] = [20., 40.];
+    let goal_y_limits: [f32; 2] = [20., 40.];
+    let goal_class: Class = Class::new(goal_x_limits, goal_y_limits);
 
-    assert_eq!(data.len(), 10);
+    let training_data_proportion: f32 = 0.7;
+
+    // Generate 10 random data points
+    let mut data: TData = [0; 100].iter().map(|_| Data::rnd(goal_class.x_limits, goal_class.y_limits)).collect();
+    let training_data_size: usize = f32::floor((data.len() as i32) as f32 * training_data_proportion) as usize;
+    let training_data: TData = data.split_off(training_data_size);
 
     let class: Class = Class::rnd();
     println!(
