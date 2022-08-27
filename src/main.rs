4440223650f22e588f8391d50ef0b87a516be339
jonbenronron <ronijonathanbk@gmail.com>
2022-08-27 10:36:28 +0300

    feat: generate random data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use rand::prelude::*;
+
+#[derive(Debug)]
+struct Data {
+    x: f32,
+    y: f32,
+}
+
+type TData = Vec<Data>;
+
 fn main() {
-    println!("Hello, world!");
+    // Generate random data
+    let mut rng = rand::thread_rng();
+
+    let buf: &[u8] = &[0; 10];
+    let data: Vec<Data> = buf.iter().map(|d| Data { x: rng.gen::<f32>(), y: rng.gen::<f32>() }).collect();
+
+    println!("{:#?}", data);
 }
