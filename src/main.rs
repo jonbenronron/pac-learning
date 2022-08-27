7d736f4add9d4069d9faf62e9b4fe3bd439881fd
jonbenronron <ronijonathanbk@gmail.com>
2022-08-27 12:11:16 +0300

    feat: simple hypothesis testing

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,65 @@ use rand::prelude::*;
 
 #[derive(Debug)]
 struct Data {
-    x: Vec<f32>,
+    x: [f32; 2],
     r: bool,
+}
+
+impl Data {
+    fn rnd () -> Self {
+        let mut rng = rand::thread_rng();
+        Data {
+            x: [rng.gen::<f32>(), rng.gen::<f32>()],
+            r: rng.gen::<bool>(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Class {
+    x_limits: [f32; 2],
+    y_limits: [f32; 2],
+}
+
+impl Class {
+    fn rnd () -> Self {
+        let mut rng = rand::thread_rng();
+        
+        let mut x: [f32; 2] = [rng.gen::<f32>(), rng.gen::<f32>()];
+        let mut y: [f32; 2] = [rng.gen::<f32>(), rng.gen::<f32>()];
+
+        x.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        y.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+        Class {
+            x_limits: x,
+            y_limits: y,
+        }
+    }
 }
 
 type TData = Vec<Data>;
 
 fn main() {
-    // Generate random data
-    let mut rng = rand::thread_rng();
+    // Generate 10 random data points
+    let data: TData = [0; 10].iter().map(|_| Data::rnd()).collect();
 
-    let buf: &[u8] = &[0; 10];
-    let data: TData = buf.iter().map(|d| Data {
-        x: Vec::from([rng.gen::<f32>(), rng.gen::<f32>()]),
-        r: rng.gen::<bool>()
-    }).collect();
+    assert_eq!(data.len(), 10);
 
     println!("{:#?}", data);
+
+    let class: Class = Class::rnd();
+    for d in data {
+        let h = hypothesis(d, Class::rnd());
+        println!(
+            "Testing hypothesis: {:#?}",
+            h,
+        );
+    }
+}
+
+fn hypothesis(x: Data, c: Class) -> bool {
+    let inside_x_limits = c.x_limits[0] < x.x[0] && x.x[0] < c.x_limits[1];
+    let inside_y_limits = c.y_limits[0] < x.x[1] && x.x[1] < c.x_limits[1];
+    x.r && inside_x_limits && inside_y_limits
 }
