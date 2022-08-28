e337d385a947fd8fad0813b6e941ba524f3eedcd
jonbenronron <ronijonathanbk@gmail.com>
2022-08-28 11:30:25 +0300

    feat: training and testing hypothesis

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rand::prelude::*;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Data {
     x: [f32; 2],
     r: bool,
@@ -43,70 +43,85 @@ impl Class {
             y_limits: y_limits,
         }
     }
-
-    fn rnd () -> Self {
-        let mut rng = rand::thread_rng();
-        
-        let mut x: [f32; 2] = [rng.gen::<f32>() * 100., rng.gen::<f32>() * 100.];
-        let mut y: [f32; 2] = [rng.gen::<f32>() * 100., rng.gen::<f32>() * 100.];
-
-        x.sort_by(|a, b| a.partial_cmp(b).unwrap());
-        y.sort_by(|a, b| a.partial_cmp(b).unwrap());
-
-        Class {
-            x_limits: x,
-            y_limits: y,
-        }
-    }
 }
 
 type TData = Vec<Data>;
+type TDelta = f32;
 
 fn main() {
     let goal_x_limits: [f32; 2] = [20., 40.];
     let goal_y_limits: [f32; 2] = [20., 40.];
     let goal_class: Class = Class::new(goal_x_limits, goal_y_limits);
 
-    let training_data_proportion: f32 = 0.7;
+    const DATA_SIZE: usize = 100;
 
-    // Generate 10 random data points
-    let mut data: TData = [0; 100].iter().map(|_| Data::rnd(goal_class.x_limits, goal_class.y_limits)).collect();
-    let training_data_size: usize = f32::floor((data.len() as i32) as f32 * training_data_proportion) as usize;
-    let training_data: TData = data.split_off(training_data_size);
+    let data: TData = [0; DATA_SIZE].iter().map(|_| Data::rnd(goal_class.x_limits, goal_class.y_limits)).collect();
+    let mut result = false;
+    let mut i = 1;
 
-    let class: Class = Class::rnd();
-    println!(
-        "Testing hypothesis class x_limits: {:?}, y_limits: {:?}: \n",
-        class.x_limits.clone(),
-        class.y_limits.clone()
-    );
-    for d in data {
-        if d.r {
-            println!(
-                "for data point x: {}, y: {}, r: {}",
-                d.x[0].clone(),
-                d.x[1].clone(),
-                d.r.clone(),
-            );
-
-            let h = hypothesis(
-                d,
-                class.x_limits.clone(),
-                class.y_limits.clone()
-            );
+    while !result && i < DATA_SIZE {
+        let mut test_data: TData = data.clone();
+        let training_data: TData = test_data.split_off(i);
         
-            println!("result: {} \n", h);
+        let hypothesis: Class = find_hypothesis(training_data.clone());
+        println!("N: {}", i);
+
+        println!(
+            "Testing hypothesis class x_limits: {:?}, y_limits: {:?}: \n",
+            hypothesis.x_limits.clone(),
+            hypothesis.y_limits.clone()
+        );
+        result = test_hypothesis(test_data, hypothesis);
+        println!("Result: {}", result);
+        i += 1;
+    }
+    
+}
+
+fn find_hypothesis(data: TData) -> Class {
+    let mut x_min = 0.;
+    let mut x_max = 100.;
+    let mut y_min = 0.;
+    let mut y_max = 100.;
+
+    for (i, d) in data.iter().enumerate() {
+        if i == 0 {
+            x_min = d.x[0];           
+            x_max = d.x[0];
+            y_min = d.x[1];
+            y_max = d.x[1];
+        } else {
+            if d.r {
+                if d.x[0] < x_min { x_min = d.x[0]; }
+                if d.x[0] > x_max { x_max = d.x[0]; }
+                if d.x[1] < y_min { y_min = d.x[1]; }
+                if d.x[1] > y_max { y_max = d.x[1]; }
+            }
         }
         
     }
+
+    Class::new([x_min, x_max], [y_min, y_max])
 }
 
-fn hypothesis(
-    x: Data,
-    hypothesis_x_limits: [f32; 2],
-    hypothesis_y_limits: [f32; 2],
+fn test_hypothesis(
+    data: TData,
+    hypothesis: Class,
 ) -> bool {
-    let inside_x_limits = hypothesis_x_limits[0] < x.x[0] && x.x[0] < hypothesis_x_limits[1];
-    let inside_y_limits = hypothesis_y_limits[0] < x.x[1] && x.x[1] < hypothesis_y_limits[1];
-    x.r && inside_x_limits && inside_y_limits
+    let delta: TDelta = 0.01;
+    let prob: f32;
+
+    let mut error: u32 = 0;
+    let n: u32 = data.len().try_into().unwrap();
+
+    for d in data {
+        if d.r {
+            if d.x[0] < hypothesis.x_limits[0] ||
+                d.x[0] > hypothesis.x_limits[1] ||
+                d.x[1] < hypothesis.y_limits[0] ||
+                d.x[1] > hypothesis.y_limits[1] { error += 1; }
+        }
+    }
+    prob = (error / n) as f32;
+    prob >= (1. - delta)
 }
